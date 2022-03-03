ae9494e7c2fc0734e56b6e35061421d8c31b2beb
Sergey <30203864+S1ealth@users.noreply.github.com>
2022-03-03 16:34:31 +0300

    chapter 3: activity lifecycle, challanges and shit

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.bignerdranch.android.geoquiz
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
 
-    private lateinit var nextButton: ImageButton
-    private lateinit var prevButton: ImageButton
+    private lateinit var nextButton: Button
 
     private lateinit var questionTextView: TextView
 
@@ -29,42 +30,94 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")
         setContentView(R.layout.activity_main)
 
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
-        prevButton = findViewById(R.id.prev_button)
         questionTextView = findViewById(R.id.question_text_view)
 
         trueButton.setOnClickListener { view: View ->
             checkAnswer(true)
+            isAnswered(currentIndex)
+            if (isAllQuestionsAnswered()) {
+                calculateResults()
+            }
         }
         falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
+            isAnswered(currentIndex)
+            if (isAllQuestionsAnswered()) {
+                calculateResults()
+            }
         }
         nextButton.setOnClickListener {
             moveToNextQuestion()
         }
-        prevButton.setOnClickListener { moveToPrevQuestion() }
-        questionTextView.setOnClickListener { moveToNextQuestion() }
+        questionTextView.setOnClickListener {
+            moveToNextQuestion()
+        }
 
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
 
     private fun moveToNextQuestion() {
         currentIndex = (currentIndex + 1) % questionBank.size
+        isAnswered(currentIndex)
         updateQuestion()
     }
 
-    private fun moveToPrevQuestion() {
-        if (currentIndex == 0) {
-            currentIndex = (questionBank.lastIndex)
+    private fun isAnswered(index: Int) {
+        val isQuestionAnswered = questionBank[index].answered
+        trueButton.isEnabled = !isQuestionAnswered
+        falseButton.isEnabled = !isQuestionAnswered
+    }
+
+    private fun isAllQuestionsAnswered(): Boolean {
+        return if(questionBank.lastIndex == currentIndex) {
+            questionBank.all { it.answered }
         } else {
-            currentIndex -= 1
+            false
         }
-        updateQuestion()
+    }
+
+    private fun calculateResults() {
+        val percentOfCorrectAnswers: Int = questionBank.filter {
+            it.isAnswerCorrect == true
+        }.size
+            .times(100)
+            .div(questionBank.size)
+        Toast.makeText(
+            this,
+            "you answered correctly on $percentOfCorrectAnswers% questions",
+            Toast.LENGTH_LONG
+        ).show()
     }
 
     private fun updateQuestion() {
@@ -74,14 +127,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = questionBank[currentIndex].answer
-
-        val messageResId = when (userAnswer) {
-            correctAnswer -> {
-                R.string.correct_toast
-            }
-            else -> {
-                R.string.incorrect_toast
-            }
+        val messageResId: Int
+        questionBank[currentIndex].answered = true
+        if (userAnswer == correctAnswer) {
+            messageResId = R.string.correct_toast
+            questionBank[currentIndex].isAnswerCorrect = true
+        } else {
+            messageResId = R.string.incorrect_toast
+            questionBank[currentIndex].isAnswerCorrect = false
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
             .show()
--- app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.kt
@@ -2,4 +2,8 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int,
+                    val answer: Boolean,
+                    var isAnswerCorrect: Boolean? = null,
+                    var answered: Boolean = false
+)
